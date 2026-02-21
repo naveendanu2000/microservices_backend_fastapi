0fd1b87fefaee4060a36acbb6276fae126c2f7f7
naveendanu2000 <naveendanu2000@gmail.com>
2026-02-22 01:22:28 +0530

    updated auth router for gateway

--- gateway/routers/authRouter.py
+++ gateway/routers/authRouter.py
@@ -4,7 +4,7 @@ from services.gateway import forward_request, AUTH_SERVICE
 router = APIRouter()
 
 
-@router.api_route("/auth/{path:path}", methods=["GET", "POST", "PUT", "DELETE"], tags=['Auth'])
+@router.api_route("/auth/{path:path}", methods=["GET", "POST"], tags=['Auth'])
 async def auth_proxy(path: str, request: Request):
     target_url = f"{AUTH_SERVICE}/{path}"
     return await forward_request(request, target_url)
