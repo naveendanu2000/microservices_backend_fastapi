e8d3896b573d14cfa1a0ed3a184f31c4f3346f55
Naveen Danu <naveendanu2000@gmail.com>
2026-02-21 14:37:47 +0530

    First interaction with strict type checking by pylance

--- auth/services/getUserCredentials.py
+++ auth/services/getUserCredentials.py
@@ -1,8 +1,10 @@
+from typing import List
 from schemas.UserCredentialsSchema import UserCredentials
+import asyncpg
 
 
-async def getUserCredentials(user: UserCredentials, conn):
-    rows = await conn.fetch(
+async def getUserCredentials(user: UserCredentials, conn: asyncpg.Connection) -> List[dict]:
+    rows: List[asyncpg.Record] = await conn.fetch(
         f"SELECT * from \"Users\".users WHERE name='{user.username}'"
     )
     return [dict(row) for row in rows]
