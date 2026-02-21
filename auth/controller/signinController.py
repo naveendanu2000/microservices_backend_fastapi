b0672f45c76b35dcc39af8671f194c71f7c3a908
Naveen Danu <naveendanu2000@gmail.com>
2026-02-21 14:42:59 +0530

    added the git ignore in the auth service

--- auth/controller/signinController.py
+++ auth/controller/signinController.py
@@ -1,7 +1,8 @@
 from services.getUserCredentials import getUserCredentials
 from schemas.UserCredentialsSchema import UserCredentials
+import asyncpg
 
 
-async def signinController(user: UserCredentials, conn):
+async def signinController(user: UserCredentials, conn: asyncpg.Connection):
     # print(user)
     return await getUserCredentials(user=user, conn=conn)
