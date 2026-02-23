2c207d0ad3fea47e05a958950059369eec8600af
Naveen Danu <naveendanu2000@gmail.com>
2026-02-23 17:25:34 +0530

    added comment router to gateway

--- gateway/main.py
+++ gateway/main.py
@@ -1,11 +1,13 @@
 from fastapi import FastAPI
 from routers.authRouter import router as authRouter
 from routers.postRouter import router as postRouter
+from routers.commentRouter import router as commentRouter
 
 app = FastAPI()
 
 app.include_router(authRouter)
 app.include_router(postRouter)
+app.include_router(commentRouter)
 
 
 @app.get("/")
